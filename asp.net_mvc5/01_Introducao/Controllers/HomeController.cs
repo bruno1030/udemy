390199f8d51fc13b53c9f09152a0a67578e26c5b
Bruno Martins <brunomartins.1030@gmail.com>
2020-05-11 01:36:10 -0300

    curso asp.net mvc5

---
+++ asp.net_mvc5/01_Introducao/Controllers/HomeController.cs
@@ -0,0 +1,72 @@
+﻿// .. bibliotecas padroes que vem
+using _01_Introducao.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace _01_Introducao.Controllers                      // .. namespace
+{
+   
+    public class HomeController : Controller             //..toda classe Controller herda de Controller
+    {
+        // GET: Home                                     
+        public ActionResult Index()              //esse funcao se chama Index, , do tipo ActionResult, e retorna uma View
+        {
+            var pessoa = new Pessoa
+            {
+                PessoaId = 1,
+                Nome = "Bruno Martins",
+                Tipo = "Analista de Sistemas"
+        };
+
+            ViewData["PessoaId"] = pessoa.PessoaId;
+            ViewData["Nome"] = pessoa.Nome;
+            ViewData["Tipo"] = pessoa.Tipo;
+
+            ViewBag.pessoaId = pessoa.PessoaId;
+            ViewBag.nome = pessoa.Nome;
+            ViewBag.tipo = pessoa.Tipo;
+
+            return View(pessoa);           // o parametro ali dentro significa que a variavel "pessoa" sera enviada para a View quando ela for carregada
+        }
+
+        public ActionResult Contato()
+        {
+            return View();
+        }
+
+        public ActionResult Sobre()
+        {
+            return View();
+        }
+
+
+        /*
+        [HttpPost]                     // esse post significa que esse metodo sera para Enviar informacoes, e nao receber
+        public ActionResult Lista(int PessoaId, string Nome, string Tipo)
+        {
+            ViewBag.PessoaId = PessoaId;
+            ViewBag.Nome = Nome;
+            ViewBag.Tipo = Tipo;
+
+            return View();
+        }
+        */
+
+
+        [HttpPost]                     // esse post significa que esse metodo sera para Enviar informacoes, e nao receber
+        public ActionResult Lista(Pessoa pessoa)
+        {
+            ViewBag.pessoaId = pessoa.PessoaId;
+            ViewBag.nome = pessoa.Nome;
+            ViewBag.tipo = pessoa.Tipo;
+
+            return View(pessoa);
+        }
+
+
+    }
+}
---
+++ asp.net_mvc5/01_Introducao/Controllers/UsuarioController.cs
@@ -0,0 +1,37 @@
+﻿using _01_Introducao.Models;
+using Microsoft.Ajax.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _01_Introducao.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        // GET: Usuario
+        public ActionResult Index()
+        {
+            var usuario = new Usuario();
+            return View(usuario);
+        }
+
+        /*
+        [HttpPost]
+        public ActionResult Usuario(Usuario usuario)
+        {
+            if (ModelState.IsValid)         // se tiver validado corretamente o usuario
+            //{
+             //   return View("Resultado", usuario);       //retornara o metodo "Resultado"
+            //}
+            return View(usuario);
+        }
+        */
+
+        public ActionResult Resultado()
+        {
+            return View();
+        }
+    }
+}
