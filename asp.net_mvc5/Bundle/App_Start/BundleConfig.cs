86afe899f28fa50dd57793f4feaed0598f682e49
Bruno Martins <brunomartins.1030@gmail.com>
2020-05-20 12:01:35 -0300

    bundle

---
+++ asp.net_mvc5/Bundle/App_Start/BundleConfig.cs
@@ -0,0 +1,38 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace Bundle
+{
+    public class BundleConfig
+    {
+        // Para obter mais informações sobre o agrupamento, visite https://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+
+            // essa linha abaixo eh uma das formas de trabalhar o Bundle.
+            // A outra forma eh la no meu Web.Config.
+            // e o que essa linha abaixo faz eh habilitar esse meu arquivo Bundle
+            // Ou seja, permito que ele pegue todos os registros abaixo,
+            // essas referencias abaixo, e compacte isso dentro da minha aplicacao
+            BundleTable.EnableOptimizations = true; 
+
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.validate*"));
+
+            // Use a versão em desenvolvimento do Modernizr para desenvolver e aprender. Em seguida, quando estiver
+            // pronto para a produção, utilize a ferramenta de build em https://modernizr.com para escolher somente os testes que precisa.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap.css",
+                      "~/Content/site.css"));
+        }
+    }
+}
---
+++ asp.net_mvc5/Bundle/Scripts/comum/setup.js
@@ -0,0 +1,2 @@
+﻿var comum = comum || {};
+comum.nome = "Bruno Martins";
